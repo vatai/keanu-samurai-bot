7dc99b02b6ce74b6163945288f31c4fc9bc70151
Emil VATAI <emil.vatai@gmail.com>
2021-05-03 20:55:04 +0900

    Call context?

--- keanu_bot.py
+++ keanu_bot.py
@@ -40,8 +40,8 @@ def convert_command(update, context):
     update.message.reply_text(reply)
 
 
-def sucks_handler(update, groups):
-    update.message.reply_text(f"{groups}!")
+def sucks_handler(update, context):
+    update.message.reply_text(f"{context()}!")
     update.message.reply_text(f"SUCKS!")
 
 
