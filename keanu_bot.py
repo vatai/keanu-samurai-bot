09d843b9537b9e4c231a0bd30a9bf52bf28c33b0
Emil VATAI <emil.vatai@gmail.com>
2021-05-03 21:27:03 +0900

    Return `bot`

--- keanu_bot.py
+++ keanu_bot.py
@@ -40,8 +40,7 @@ def convert_command(update, context):
     update.message.reply_text(reply)
 
 
-def sucks_handler(update, context, groups=None):
-    update.message.reply_text(f"{groups}!")
+def sucks_handler(bot, update, context, groups=None):
     update.message.reply_text(f"{groups} -- SUCKS!")
 
 
