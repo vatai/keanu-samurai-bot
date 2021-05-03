adee114be5dbba32a76094eb2b8428d9a456cc71
Emil VATAI <emil.vatai@gmail.com>
2021-05-03 21:29:25 +0900

    Remove bot

--- keanu_bot.py
+++ keanu_bot.py
@@ -40,7 +40,7 @@ def convert_command(update, context):
     update.message.reply_text(reply)
 
 
-def sucks_handler(bot, update, context, groups=None):
+def sucks_handler(update, context, groups=None):
     update.message.reply_text(f"{groups} -- SUCKS!")
 
 
