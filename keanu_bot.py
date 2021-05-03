3b7def2f43133b0929f167a2b4588efbc0482051
Emil VATAI <emil.vatai@gmail.com>
2021-05-03 20:40:05 +0900

    Add 5 args to sucks

--- keanu_bot.py
+++ keanu_bot.py
@@ -40,7 +40,7 @@ def convert_command(update, context):
     update.message.reply_text(reply)
 
 
-def sucks_handler(update, context):
+def sucks_handler(bot, update, context, groups):
     update.message.reply_text(f"{context}!")
     update.message.reply_text(f"SUCKS!")
 
